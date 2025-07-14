e150fcb23b20ff6479fad1e1473424c2173f8601
Nuria Campo <nuria.campo7@gmail.com>
2025-07-14 19:29:15 +0200

    add filters

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,16 +1,21 @@
-function Filters() {
-  return (
-    <div>
-      <select name="country" id="">
-        <option value="">Estados Unidos</option>
-        <option value="">Kenia</option>
-      </select>
-      <select name="field" id="">
-        <option value="">Activismo</option>
-        <option value="">Ecología</option>
-      </select>
-    </div>
-  );
-}
-
+const Filters = ({ countries, fields, onChange, selected }) => (
+  <div className="filters">
+    <select name="country" value={selected.country} onChange={onChange}>
+      <option value="">Todos los países</option>
+      {countries.map((c, i) => (
+        <option key={i} value={c}>
+          {c}
+        </option>
+      ))}
+    </select>
+    <select name="field" value={selected.field} onChange={onChange}>
+      <option value="">Todas las áreas</option>
+      {fields.map((f, i) => (
+        <option key={i} value={f}>
+          {f}
+        </option>
+      ))}
+    </select>
+  </div>
+);
 export default Filters;
